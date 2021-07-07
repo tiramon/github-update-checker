6759b15081e8f469fca61a15a519501a78220c81
tiramon <github@checkspam.de>
2021-07-07 13:53:21 +0200

    remove unneeded setter on models

--- src/main/java/de/tiramon/github/update/model/GitHubAsset.java
+++ src/main/java/de/tiramon/github/update/model/GitHubAsset.java
@@ -3,33 +3,21 @@ package de.tiramon.github.update.model;
 import com.google.gson.annotations.SerializedName;
 
 public class GitHubAsset {
-	String name;
-	long size;
+	private String name;
+	private long size;
 	@SerializedName("browser_download_url")
-	String browserDownloadUrl;
+	private String browserDownloadUrl;
 
 	public String getName() {
 		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	public long getSize() {
 		return size;
 	}
 
-	public void setSize(long size) {
-		this.size = size;
-	}
-
 	public String getBrowserDownloadUrl() {
 		return browserDownloadUrl;
-	}
-
-	public void setBrowserDownloadUrl(String browserDownloadUrl) {
-		this.browserDownloadUrl = browserDownloadUrl;
 	}
 
 }
--- src/main/java/de/tiramon/github/update/model/GitHubReleaseInformation.java
+++ src/main/java/de/tiramon/github/update/model/GitHubReleaseInformation.java
@@ -6,43 +6,27 @@ import com.google.gson.annotations.SerializedName;
 
 public class GitHubReleaseInformation {
 	@SerializedName("html_url")
-	String htmlUrl;
+	private String htmlUrl;
 	@SerializedName("tag_name")
-	String tagName;
+	private String tagName;
 	@SerializedName("published_at")
-	Date publishedAt;
-	GitHubAsset[] assets;
+	private Date publishedAt;
+	private GitHubAsset[] assets;
 
 	public String getHtmlUrl() {
 		return htmlUrl;
-	}
-
-	public void setHtmlUrl(String htmlUrl) {
-		this.htmlUrl = htmlUrl;
 	}
 
 	public String getTagName() {
 		return tagName;
 	}
 
-	public void setTagName(String tagName) {
-		this.tagName = tagName;
-	}
-
 	public Date getPublishedAt() {
 		return publishedAt;
 	}
 
-	public void setPublishedAt(Date publishedAt) {
-		this.publishedAt = publishedAt;
-	}
-
 	public GitHubAsset[] getAssets() {
 		return assets;
-	}
-
-	public void setAssets(GitHubAsset[] assets) {
-		this.assets = assets;
 	}
 
 }
